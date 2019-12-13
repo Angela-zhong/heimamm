4d6818f2257f95867da2d627917111f3f1490aab
Angela-zhong <1305423949@qq.com>
2019-12-13 11:25:40 +0800

    初始化-整合路由

---
+++ src/router/router.js
@@ -0,0 +1,16 @@
+// 导入
+import Vue from "vue"
+// 导包
+import vueRouter from vue-router
+//use下注册
+Vue.use(vueRouter) 
+
+// 规则
+const router=[]
+// 创建
+const router=new VueRouter({
+    
+   router
+})
+// 暴露出去
+export default router
