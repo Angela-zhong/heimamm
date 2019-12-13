af322d310814f730081cc840ff9e704041f5675f
Angela-zhong <1305423949@qq.com>
2019-12-13 11:27:45 +0800

    初始化-整合element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router.vue'
+import ElementUI from 'element-ui';
+// 导入 Element-ui的样式
+import 'element-ui/lib/theme-chalk/index.css';
+// 注册 Element-ui
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
